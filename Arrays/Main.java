25c11bc826c66963ea4685b41e37c772d770aea4
PALLEBOINASAHITHIYADAV <sahithisuresh14@gmail.com>
2025-07-29 21:57:29 +0530

    Added program to insert an element in an array

---
+++ Arrays/Main.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        int newelement = 25;
+        int index = 2;
+
+        arr = Arrays.copyOf(arr, arr.length + 1);
+
+        for (int i = arr.length - 1; i > index; i--) {
+            arr[i] = arr[i - 1];
+        }
+
+        arr[index] = newelement;
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
