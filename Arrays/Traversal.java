b970f79a6a1bb02c8c162c7e8c6c1ca5189db6b2
PALLEBOINASAHITHIYADAV <sahithisuresh14@gmail.com>
2025-07-31 15:12:19 +0530

    Added a Array traversal program

---
+++ Arrays/Traversal.java
@@ -0,0 +1,9 @@
+public class Traversal {
+    public static void main(String args[]){
+        int[] arr = {1,3,4,5,6};
+        for(int i =0;i< arr.length;i++){
+              System.out.println("Element at index " +i+ " is "  + arr[i]);
+        }
+    }
+    
+}
